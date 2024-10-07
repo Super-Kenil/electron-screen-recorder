1d015f5fdc77ce379651ab2596239960e7ff6b98
Super-Kenil <kenilsudani.blog@gmail.com>
2024-10-07 20:54:32 +0530

    WIP: Start and stop recording

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { app, BrowserWindow, desktopCapturer, ipcMain } from 'electron'
+import { app, BrowserWindow, desktopCapturer, dialog, ipcMain } from 'electron'
+import { writeFile } from 'fs'
 import path from 'path'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  app.quit();
+  app.quit()
 }
 
 const createWindow = () => {
@@ -16,40 +17,40 @@ const createWindow = () => {
       nodeIntegration: true,
       nodeIntegrationInSubFrames: true,
     },
-  });
+  })
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  }  else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  mainWindow.webContents.openDevTools()
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
@@ -58,3 +59,16 @@ ipcMain.handle('GET_INPUT_SOURCES', async () => {
   return inputSources
 })
 
+ipcMain.handle('STOP_SCREEN_RECORDING', async (event, source: Electron.DesktopCapturerSource, buffer: Buffer) => {
+
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: 'Save video',
+    defaultPath: `${source.name}-recording-${Date.now()}.webm`
+  })
+
+  if (filePath) {
+    writeFile(filePath, buffer, () => {
+      alert('Video saved successfully')
+    })
+  }
+})
--- src/screens/Recording/index.tsx
+++ src/screens/Recording/index.tsx
@@ -1,26 +1,28 @@
-import { desktopCapturer, Menu } from 'electron'
+import { desktopCapturer, dialog, Menu } from 'electron'
+import { writeFile } from 'fs'
 import { chrome } from 'process'
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { FcOpenedFolder, FcVideoCall } from "react-icons/fc"
 import { Link } from 'react-router-dom'
-// const { desktopCapturer, remote } = require('electron');
-
-// const { dialog, Menu } = remote
-
 
 const RecordingScreen = () => {
 
   const [videoSelectButtonText, setVideoSelectButtonText] = useState('Choose a Video Source')
   const [videoSources, setVideoSources] = useState<Electron.DesktopCapturerSource[]>([])
   const [selectedSource, setSelectedSource] = useState<Electron.DesktopCapturerSource>()
+  const [isRecording, setIsRecording] = useState(false)
+
+  const videoElRef = useRef<HTMLVideoElement>()
+  const streamRecorderRef = useRef<MediaRecorder>(null)
 
   console.log('window.electron::: ', window.electron)
 
-  const videoElRef = useRef<HTMLVideoElement>()
+  const recordedChunks: Blob[] = []
+
 
   const getVideoSources = async () => {
 
-    window.electron.ipcRenderer.invoke('GET_INPUT_SOURCES',)
+    window.electron.ipcRenderer.invoke('GET_INPUT_SOURCES')
       .then((sources: Electron.DesktopCapturerSource[]) => {
         console.log("🚀 ~ .then ~ sources:", sources)
         setVideoSources(sources)
@@ -54,11 +56,37 @@ const RecordingScreen = () => {
       }
     }
 
-    const stream = await navigator.mediaDevices.getUserMedia(constraints)
+    const stream: MediaStream = await navigator.mediaDevices.getUserMedia(constraints)
 
     videoElRef.current.srcObject = stream
     videoElRef.current.play()
 
+    const options: MediaRecorderOptions = { mimeType: 'video/webm; codecs=vp9' }
+    streamRecorderRef.current = new MediaRecorder(stream, options)
+
+    streamRecorderRef.current.ondataavailable = (event: BlobEvent) => {
+      recordedChunks.push(event.data)
+    }
+
+    streamRecorderRef.current.onstop = handleStopRecording
+  }
+
+  const handleStopRecording = async () => {
+    const blob = new Blob(recordedChunks, {
+      type: 'video/webm; codecs=vp9'
+    })
+
+    const buffer = Buffer.from(await blob.arrayBuffer())
+
+
+
+  }
+
+  const handleStartRecording = () => {
+    if (selectedSource) {
+      streamRecorderRef.current?.start()
+      setIsRecording(true)
+    }
   }
 
   return (
@@ -71,8 +99,13 @@ const RecordingScreen = () => {
         <video ref={videoElRef} className='max-w-[720px]' />
 
         <div className="flex items-center justify-center gap-2">
-          <button disabled={!videoSources.length} className="px-3 py-1.5 rounded-md bg-emerald-100 text-emerald-950 flex items-center gap-2 border-emerald-500 border-2 disabled:bg-gray-300 disabled:border-gray-400 disabled:text-gray-900"><FcVideoCall size={18} /> Start</button>
-          <button disabled={!videoSources.length} className="px-3 py-1.5 rounded-md bg-red-100 text-red-950 flex items-center gap-2 border-red-500 border-2 disabled:bg-gray-300 disabled:border-gray-400 disabled:text-gray-900"> 🛑🫷 Stop</button>
+
+          {isRecording ?
+            <button className="px-3 py-1.5 rounded-md bg-red-100 text-red-950 flex items-center gap-2 border-red-500 border-2 disabled:bg-gray-300 disabled:border-gray-400 disabled:text-gray-900"> 🛑🫷 Stop</button>
+            :
+            <button disabled={!selectedSource} className="px-3 py-1.5 rounded-md bg-emerald-100 text-emerald-950 flex items-center gap-2 border-emerald-500 border-2 disabled:bg-gray-300 disabled:border-gray-400 disabled:text-gray-900"><FcVideoCall size={18} /> Start</button>
+          }
+
         </div>
         <hr className='h-2 m-2' />
 
